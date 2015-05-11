4bbaab60d78df20fb386ba217f6629919b702d1e
TobiasKl <tobiasklaiber@gmx.de>
2015-05-11 12:49:53 +0200

    removed unused import

--- src/de/uniHannover/imes/igtIf/util/FileSystemUtil.java
+++ src/de/uniHannover/imes/igtIf/util/FileSystemUtil.java
@@ -4,12 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
