2f8bc1af0941ef2bb990c43968330c253f11ad5f
TobiasKl <tobiasklaiber@gmx.de>
2015-05-31 13:27:46 +0200

    added javadoc

--- src/de/uniHannover/imes/igtIf/communication/ExceptionHandlerComThreads.java
+++ src/de/uniHannover/imes/igtIf/communication/ExceptionHandlerComThreads.java
@@ -4,23 +4,37 @@ import java.lang.Thread.UncaughtExceptionHandler;
 
 import com.kuka.roboticsAPI.applicationModel.tasks.ITaskLogger;
 
+/**
+ * Exception handler for the igtl-communication threads. It's purpose is just to
+ * print all uncaugth exceptions from the according threads.
+ */
 public class ExceptionHandlerComThreads implements UncaughtExceptionHandler {
 
-	private ITaskLogger log;
+    // **************************Components*********************/
+    /** Logger for printing the exceptions. */
+    private ITaskLogger log;
 
-	public ExceptionHandlerComThreads(final ITaskLogger logger) {
-		if (null == logger) {
-			throw new NullPointerException("Argument is null");
-		} else {
-			log = logger;
-		}
+    /**
+     * Creates an exception handler object.
+     * 
+     * @param logger
+     *            the logger, used for printing the exception caught in the
+     *            according threads.
+     */
+    public ExceptionHandlerComThreads(final ITaskLogger logger) {
+	if (null == logger) {
+	    throw new NullPointerException("Argument is null");
+	} else {
+	    log = logger;
 	}
+    }
 
-	@Override
-	public void uncaughtException(Thread t, Throwable e) {
-		log.error("An uncaught exception occured in the thread "
-				+ t.getClass().getSimpleName(), e);
+ 
+    @Override
+    public final void uncaughtException(final Thread t, final Throwable e) {
+	log.error("An uncaught exception occured in the thread "
+		+ t.getClass().getSimpleName(), e);
 
-	}
+    }
 
 }
